4403a5cbde1a3a1b65880f5b215fd92f7cce03a5
kumpulanremaja <47444671+kumpulanremaja@users.noreply.github.com>
2019-11-15 22:13:44 +0700

    Update tembak.php

--- tembak.php
+++ tembak.php
@@ -2,8 +2,11 @@
 date_default_timezone_set('Asia/Jakarta');
 include "function.php";
 echo color("red"," ===========================\n");
+echo color("red","| Claim Voucher Gojek|\n");
 echo color("red","| Auto create Gojek X Redeem voucher |\n");
-echo color("red","| Creator : Calon Sarjana            |\n");
+echo color("red","| github: kumpulanremaja |\n");
+echo color("red","| fanspage :4kumpulanremaja|\n");
+echo color("red","| Creator : kumpulanremaja.com          |\n");
 echo "| Version : Premium X pertalite      |\n";
 echo "| Time    :".date('[d-m-Y] [H:i:s]')."   |\n";
 echo " ===========================\n";
